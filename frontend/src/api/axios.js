7ce412ecade2407a27a487d71a351e71ae643258
AyushJha2008 <ayushsjha2008@gmail.com>
2026-03-07 09:39:12 +0530

    feat: update API base URL to include '/api' for correct endpoint access

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: " https://course-scheduler-xouz.onrender.com",
+  baseURL: " https://course-scheduler-xouz.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
